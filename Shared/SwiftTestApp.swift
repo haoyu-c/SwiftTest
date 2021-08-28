1d98ff046305f991fe6ea334bcecf8589a6f9d33
chenhaoyu.1999 <chenhaoyu.1999@bytedance.com>
2021-08-28 17:02:15 +0800

    Initial Commit

---
+++ Shared/SwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestApp.swift
+//  Shared
+//
+//  Created by chenhaoyu.1999 on 2021/8/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
